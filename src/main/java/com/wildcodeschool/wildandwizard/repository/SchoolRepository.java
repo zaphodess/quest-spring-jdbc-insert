bd689377561a1df70bd7893d215854d70b008e56
zaphodess <monika.messerer@telekom.de>
2020-01-27 11:19:20 +0100

    Found the bugger now

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,5 +1,14 @@
 package com.wildcodeschool.wildandwizard.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import util.JdbcUtils;
+
 import com.wildcodeschool.wildandwizard.entity.School;
 
 public class SchoolRepository {
@@ -9,8 +18,41 @@ public class SchoolRepository {
     private final static String DB_PASSWORD = "Horcrux4life!";
 
     public School save(String name, Long capacity, String country) {
+      Connection connection = null;
+      PreparedStatement statement = null;
+      ResultSet generatedKeys = null;
+      try {
+          connection = DriverManager.getConnection(
+                  DB_URL, DB_USER, DB_PASSWORD
+          );
+          statement = connection.prepareStatement(
+                  "INSERT INTO school (name, capacity, country) VALUES (?, ?, ?)",
+                  Statement.RETURN_GENERATED_KEYS
+          );
+          statement.setString(1, name);
+          statement.setLong(2, capacity);
+          statement.setString(3, country);
 
-        // TODO : insert a new school in database
+
+          if (statement.executeUpdate() != 1) {
+              throw new SQLException("failed to insert data");
+          }
+
+          generatedKeys = statement.getGeneratedKeys();
+
+          if (generatedKeys.next()) {
+              Long id = generatedKeys.getLong(1);
+              return new School(id, name, capacity, country);
+          } else {
+              throw new SQLException("failed to get inserted id");
+          }
+      } catch (SQLException e) {
+          e.printStackTrace();
+      } finally {
+          JdbcUtils.closeResultSet(generatedKeys);
+          JdbcUtils.closeStatement(statement);
+          JdbcUtils.closeConnection(connection);
+      }
         return null;
     }
 }
